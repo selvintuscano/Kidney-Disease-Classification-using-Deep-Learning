3652bf2f0462a96a03c3b5012bcfea8e94f6c973
Selvin Tuscano <selvintuscano@Selvins-MacBook-Air.local>
2025-07-30 16:34:02 -0400

    Model evaluation added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from cnnClassifier import logger
+import mlflow
 from cnnClassifier.pipeline.stage01_dataingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage03_model_training import ModelTrainingPipeline
-
+from cnnClassifier.pipeline.stage04_model_evaluation import EvaluationPipeline
 
 
 
@@ -37,6 +38,18 @@ try:
    model_trainer = ModelTrainingPipeline()
    model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
